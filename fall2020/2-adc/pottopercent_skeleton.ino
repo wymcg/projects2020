99daf44639dd353b0d6c363af16f5fbe356fcab6
wymcg <3410869+wymcg@users.noreply.github.com>
2021-04-12 21:55:53 -0400

    Added skeleton code for pottopercent.ino

---
+++ fall2020/2-adc/pottopercent_skeleton.ino
@@ -0,0 +1,18 @@
+#define adcMax 1023
+#define poll 1000
+
+void setup()
+{
+    //start serial
+
+}
+
+void loop()
+{
+  //map analogread to a percent value
+ 
+  //print percent value to serial monitor
+
+  //wait to check again
+
+}
